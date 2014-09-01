f369ce7a17df70b5fe70c8ca75a9bc47d65f6187
Arash Bakhtiari <bakhtiar@in.tum.de>
2014-09-01 17:58:54 +0200

    Added the google c++ testing framework.

--- configure.sh
+++ configure.sh
@@ -2,5 +2,6 @@ mkdir -p build; rm -rf build/*;
 cd build;
 cmake .. \
 -DCMAKE_C_COMPILER=clang \
--DCMAKE_CXX_COMPILER=clang++; \
+-DCMAKE_CXX_COMPILER=clang++ \
+-DBUILD-GTEST-UNITTEST=ON \
 # -DCMAKE_PREFIX_PATH=~/.local
--- tests/unittests/semilag/semilag_tst1.cpp
+++ tests/unittests/semilag/semilag_tst1.cpp
@@ -1,9 +1,8 @@
 #include <semilag.hpp>
+#include <gtest/gtest.h>
 
-int main(int argc, char *argv[])
-{
+TEST(SemilagTest, GenerateRegGridPoints){
   size_t N = 10;
   std::vector<double> coord;
   coord = semilag::gen_reg_grid_points<double>(N);
-  return 0;
 }
