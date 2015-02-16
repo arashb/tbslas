2f505d5381ebe37df6a4477131ff227d71002afe
Arash Bakhtiari <bakhtiar@in.tum.de>
2015-02-16 14:24:45 -0600

    FIX: Compilation issue.

---
+++ src/utils/types.h
@@ -0,0 +1,23 @@
+#ifndef SRC_UTILS_TYPES_H
+#define SRC_UTILS_TYPES_H
+
+namespace tbslas {
+
+// point distribution types
+enum DistribType{
+  UnifGrid,
+  RandUnif,
+  RandGaus,
+  RandElps,
+  RandSphr
+};
+
+// boundary condition types
+// enum BoundaryConditionType{
+//   BC_FreeSpace,
+//   BC_Periodic
+// };
+
+}
+
+#endif /* TYPES_H */
